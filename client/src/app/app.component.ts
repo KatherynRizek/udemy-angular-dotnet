b70443f54943be3885ba3a128caea9b08b7a59d6
Katheryn Rizek <katheryn.rizek@excella.com>
2020-11-13 14:40:59 -0500

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 
-//onInit is a lifecycle hook that is called after Angular has initialized all data-bound properties 
+//onInit is a lifecycle hook that is called after Angular has initialized all data-bound properties
 //of a directive. Define an ngOnInit() method to handle any additional initialization tasks.
-export class AppComponent implements OnInit{
-  title = 'Dating App';
-  users: any;
+export class AppComponent implements OnInit {
+    title = 'Dating App';
+    users: any;
 
-  constructor(private http: HttpClient) {} 
+    constructor(private accountService: AccountService) {}
 
-  ngOnInit() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response =>{
-      this.users = response;
-    }, error => {
-      console.log(error);
-    });
-  }
+    ngOnInit() {
+        this.setCurrentUser();
+    }
+
+    setCurrentUser() {
+        const user: User = JSON.parse(localStorage.getItem('user'));
+        this.accountService.setCurrentUser(user);
+    }
 }
