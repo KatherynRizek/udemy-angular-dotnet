703be52c9a7f2aaece59b4900ad9a247135fc52a
Katheryn Rizek <katheryn.rizek@excella.com>
2020-11-23 14:37:01 -0500

    End of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,23 +1,27 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Extensions
 {
-    public static class ApplicationServiceExtensions
+  public static class ApplicationServiceExtensions
+  {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
-        {
-            services.AddScoped<ITokenService, TokenService>(); //most appropriate for an HTTP request
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
+      services.AddScoped<ITokenService, TokenService>(); //most appropriate for an HTTP request
+      services.AddScoped<IUserRepository, UserRepository>();
+      services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+      services.AddDbContext<DataContext>(options =>
+      {
+        options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+      });
 
-            return services;
-        }
+      return services;
     }
+  }
 }
